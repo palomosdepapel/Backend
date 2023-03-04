64d355e09cf2b0eedf579576decf12748ec80714
Juan Carlos Huertas <jhuertas2016@gmail.com>
2023-03-04 18:27:32 -0500

    Update ProductManager2.js

---
+++ Desafio-1/ProductManager2.js
@@ -0,0 +1,9 @@
+    title: 'Reloj Claude Bernard Análogo',
+    description: 'Reloj claude bernard analogo, para hombre, tablero redondo colores blanco y negro, estilo romanos, pulso cuero color negro, calendario , Diámetro: 39.00mm , Resistencia al agua: 3 bares',
+    thumb: 'https://kevins.com.co/img/productos/R206000312.jpg',
+    price: 753000,
+    code: '3',
+    stock: 5,
+})
+console.log('All products: ',products.getProducts())
+console.log('By ID: ',products.getProductById(1))
