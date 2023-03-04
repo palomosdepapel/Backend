6d681f848c8230ba9db82929e99c313e9a908dc6
Juan Carlos Huertas <jhuertas2016@gmail.com>
2023-03-04 09:30:35 -0500

    Adjust

--- Desafio-1/productManager.js
+++ Desafio-1/productManager.js
@@ -1,5 +1,5 @@
 //https://stackoverflow.com/questions/56186391/how-to-create-a-class-that-will-manage-goods-in-js
-//https://stackoverflow.com/questions/72410381/array-management-in-javascript
+
 
 class Products {
   constructor(title, price, description, thumb, code, stock) {
@@ -20,9 +20,16 @@ let puma = new Products("Puma", "description new-shoes", 150, "img/puma.png", 50
 let jordan = new Products("Jordan", "description outlet-shoes", 170, "img/jordan.png", 504, 4);
 let converse = new Products("Converse", "description outlet-shoes", 70, "img/convrse.png", 505, 2);
 let nikeAirMax = new Products("Nike Air Max", "description shoes", 200, "img/nikeAirMax.png", 506, 6);
-let newBal = new Products("New Balance 990", "description new-shoes", 179, "img/newBal.png", 507, 2);
 
-let arrGoods2 = [nike,adidas,puma,jordan,nikeAirMax,converse,newBal];
+
+let arrGoods2 = [nike,adidas,puma,jordan,nikeAirMax,converse];
 
 console.log(arrGoods1);
 console.log(arrGoods2);
+
+let newBal = new Products("New Balance 990", "description new-shoes", 179, "img/newBal.png", 507, 2);
+arrGoods2.push(newBal);
+
+console.log(arrGoods2);
+
+
