a64eefbddc26e99d38a235cf59ea0a3afcfadfe5
Juan Carlos Huertas <jhuertas2016@gmail.com>
2023-03-03 20:57:07 -0500

    Update productManager.js

--- Desafio-1/productManager.js
+++ Desafio-1/productManager.js
@@ -1,47 +1,98 @@
-const items = [
-  {
-    name: "Nike",
-    price: 100,
-    desc: "new-shoes",
-    img: "https://www.iconsdb.com/icons/preview/red/nike-xxl.png",
-  },
-  {
-    name: "Adidas",
-    price: 120,
-    desc: "classic-shoes",
-    img: "https://www.vouchercodes.co.uk/static/v10/images/merchant/logo/128px/1524_160119151356.png",
-  },
-  {
-    name: "Puma",
-    price: 150,
-    desc: "new-shoes",
-    img: "https://www.freepnglogos.com/uploads/puma-logo-png-1.png",
-  },
-  {
-    name: "Jordan",
-    price: 170,
-    desc: "outlet-shoes",
-    img: "https://www.svgimages.com/svg-image/s8/air-jordan-logo-256x256.png",
-  },
-  {
-    name: "Converse",
-    price: 70,
-    desc: "outlet-shoes",
-    img: "https://d13genyhhfmqry.cloudfront.net/widget/mp_340806_2019-04-06-01-57-52-000851.jpg",
-  },
-  {
-    name: "Nike Air Max",
-    price: 200,
-    desc: "shoes",
-    img: "https://www.iconsdb.com/icons/preview/red/nike-xxl.png",
-  },
-  {
-    name: "New Balance 990",
-    price: 179,
-    desc: "new-shoes",
-    img: "https://www.vouchercodes.co.uk/static/v10/images/merchant/logo/128px/4484_160309151253.png",
-  },
-];
+// const items = [
+//   {
+//     name: "Nike",
+//     price: 100,
+//     desc: "new-shoes",
+//     img: "https://www.iconsdb.com/icons/preview/red/nike-xxl.png",
+//   },
+//   {
+//     name: "Adidas",
+//     price: 120,
+//     desc: "classic-shoes",
+//     img: "https://www.vouchercodes.co.uk/static/v10/images/merchant/logo/128px/1524_160119151356.png",
+//   },
+//   {
+//     name: "Puma",
+//     price: 150,
+//     desc: "new-shoes",
+//     img: "https://www.freepnglogos.com/uploads/puma-logo-png-1.png",
+//   },
+//   {
+//     name: "Jordan",
+//     price: 170,
+//     desc: "outlet-shoes",
+//     img: "https://www.svgimages.com/svg-image/s8/air-jordan-logo-256x256.png",
+//   },
+//   {
+//     name: "Converse",
+//     price: 70,
+//     desc: "outlet-shoes",
+//     img: "https://d13genyhhfmqry.cloudfront.net/widget/mp_340806_2019-04-06-01-57-52-000851.jpg",
+//   },
+//   {
+//     name: "Nike Air Max",
+//     price: 200,
+//     desc: "shoes",
+//     img: "https://www.iconsdb.com/icons/preview/red/nike-xxl.png",
+//   },
+//   {
+//     name: "New Balance 990",
+//     price: 179,
+//     desc: "new-shoes",
+//     img: "https://www.vouchercodes.co.uk/static/v10/images/merchant/logo/128px/4484_160309151253.png",
+//   },
+// ];
 
-console.log(items)
+// console.log(items)
 //https://stackoverflow.com/questions/56186391/how-to-create-a-class-that-will-manage-goods-in-js
+//https://stackoverflow.com/questions/72410381/array-management-in-javascript
+
+
+// Realizar una clase "ProductManager" que gestione un conjunto de productos
+class ProductManager {
+  // Debe crearse desde su constructor con el elemento products, el cual será un arreglo vacío
+  // constructor() {
+  //   this.products = [];
+  // }
+  constructor(title, description, price, thumbnail, code, stock){
+    this.title = title;
+    this.description = description;
+    this.price = price;    
+    this.thumbnail = thumbnail;
+    this.code = code;
+    this.stock = stock;
+  }
+
+  addProduct(product) {
+    // if (!this.products.length) {
+    //   this.products.push({ ...product, quantity: 1 });
+    // } else {
+    //   const index = this.products.findIndex(item => {
+    //     return item.productId === product.productId;
+    //   });
+    //   if (index > -1) {
+    //     ++this.products[index].quantity;
+    //   } else {
+    //     this.products.push({ ...product, quantity: 1 });
+    //   }
+    // }
+  }
+  
+  getProducts() {
+    return this.products;
+  }
+
+}
+
+
+
+const products = new ProductManager();
+
+products.addProduct({ productId: 1 });
+products.addProduct({ productId: 3 });
+products.addProduct({ productId: 2 });
+products.addProduct({ productId: 3 });
+products.addProduct({ productId: 2 });
+
+//Debe contar con un método "GetProducts" el cual debe devolver el arreglo con todos los productos creados hasta el momento
+console.log(products.getProducts());
