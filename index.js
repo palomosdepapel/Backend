19e29a21e5a2848a31720bf746e6ffade3c3ba48
Juan Carlos Huertas <jhuertas2016@gmail.com>
2023-03-18 17:34:35 -0500

    endpoints response

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-const http = require('http')
+
+import express  from 'express'
+
+const app = express()
+
+const PORT = 8080
+
+// ruta
+app.get('/saludo', (req, res)=>{
+    res.send("Hola profe")
+} )
+
+// ejercicio
+app.get('/bienvenido', (req, res)=>{
+    res.send('<h1 style="color:blue">Hola mundo</h1>')
+} )
+
+// ejercicio
+app.get('/usuario', (req, res)=>{
+    res.send({nombre:'juan', apellido:'huertas', email:'jhuertas@dominio.com', edad: 55})
+} )
+
+app.listen(PORT , () =>{
+    console.log('Servidor iniciado en el puerto ' + PORT)
+})
+
+
+
+/* const http = require('http')
 //petición respuesta
 const server = http.createServer((req, res) => {
     res.end('Hola mundo, mi primer servidor...')
@@ -11,3 +39,4 @@ const PORT = 8080
 server.listen(PORT , () =>{
     console.log('Servidor iniciado en el puerto ' + PORT)
 })
+ */
